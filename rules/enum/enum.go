2474f7c1b017f52cff32c887727849c02a2c0b55
Dmitry Shemin <shemindmitry@gmail.com>
2022-06-25 10:34:14 +0700

    SERV-26 Remove checker

--- rules/enum/enum.go
+++ rules/enum/enum.go
@@ -6,7 +6,6 @@ import (
 
 	jschema "github.com/jsightapi/jsight-schema-go-library"
 	"github.com/jsightapi/jsight-schema-go-library/bytes"
-	"github.com/jsightapi/jsight-schema-go-library/errors"
 	"github.com/jsightapi/jsight-schema-go-library/fs"
 	"github.com/jsightapi/jsight-schema-go-library/internal/lexeme"
 )
@@ -40,13 +39,9 @@ func FromFile(f *fs.File) *Enum {
 
 func (e *Enum) Len() (uint, error) {
 	e.computeLengthOnce.Do(func() {
-		e.length, e.computeLengthErr = e.computeLength()
+		e.length, e.computeLengthErr = newScanner(e.file, scannerComputeLength).Length()
 	})
 	return e.length, e.computeLengthErr
-}
-
-func (e *Enum) computeLength() (length uint, err error) {
-	return newScanner(e.file, scannerComputeLength).Length()
 }
 
 // Check checks that enum is valid.
@@ -87,84 +82,4 @@ func (e *Enum) doCompile() (err error) {
 		}
 	}
 	return nil
-}
-
-type enumChecker struct {
-	// stateFunc a function for running a state machine (the current state of the
-	// state machine).
-	stateFunc func(lexeme.LexEvent)
-}
-
-func newEnumChecker() *enumChecker {
-	l := &enumChecker{}
-	l.stateFunc = l.begin
-	return l
-}
-
-// Check checks the lexeme sequence to make sure it is an enum. When any error is
-// detected, sends DocumentError into a panic.
-func (l *enumChecker) Check(lex lexeme.LexEvent) {
-	defer lexeme.CatchLexEventError(lex)
-	l.stateFunc(lex)
-}
-
-// begin of array "["
-func (l *enumChecker) begin(lex lexeme.LexEvent) {
-	switch lex.Type() {
-	case lexeme.NewLine:
-		return
-	case lexeme.ArrayBegin:
-		l.stateFunc = l.arrayItemBeginOrArrayEnd
-	default:
-		panic(errors.ErrEnumArrayExpected)
-	}
-}
-
-// arrayItemBeginOrArrayEnd handles beginning of array item begin or the end of array.
-// ex: [1 <--
-// ex: [" <--
-// ex: ] <--
-func (l *enumChecker) arrayItemBeginOrArrayEnd(lex lexeme.LexEvent) {
-	switch lex.Type() {
-	case lexeme.NewLine:
-		return
-	case lexeme.ArrayItemBegin:
-		l.stateFunc = l.literal
-	case lexeme.ArrayEnd:
-		l.stateFunc = l.afterEndOfEnum
-	default:
-		panic(errors.ErrLoader)
-	}
-}
-
-// literal handles the literal value inside array.
-func (l *enumChecker) literal(lex lexeme.LexEvent) {
-	switch lex.Type() {
-	case lexeme.LiteralBegin:
-		return
-	case lexeme.LiteralEnd:
-		l.stateFunc = l.arrayItemEnd
-	default:
-		panic(errors.ErrIncorrectArrayItemTypeInEnumRule)
-	}
-}
-
-// arrayItemEnd handles the end of array item.
-func (l *enumChecker) arrayItemEnd(lex lexeme.LexEvent) {
-	switch lex.Type() {
-	case lexeme.NewLine:
-		return
-	case lexeme.ArrayItemEnd:
-		l.stateFunc = l.arrayItemBeginOrArrayEnd
-	default:
-		panic(errors.ErrLoader)
-	}
-}
-
-// afterEndOfEnum the method should not be called during normal operation. Ensures
-// that the checker will not continue to work after the load is complete.
-func (*enumChecker) afterEndOfEnum(lex lexeme.LexEvent) {
-	if lex.Type() != lexeme.NewLine {
-		panic(errors.ErrUnnecessaryLexemeAfterTheEndOfEnum)
-	}
 }
